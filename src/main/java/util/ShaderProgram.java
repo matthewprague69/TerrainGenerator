912d3e9e24af5471163804c8a7adfb67620a9764
matthewprague69 <matous.prazakg@gmail.com>
2026-01-21 19:56:47 +0100

    Add lighting and soften shadow sampling

---
+++ src/main/java/util/ShaderProgram.java
@@ -0,0 +1,67 @@
+package util;
+
+import org.lwjgl.opengl.GL20;
+
+import java.nio.FloatBuffer;
+
+import static org.lwjgl.opengl.GL20.*;
+
+public class ShaderProgram {
+    private final int programId;
+
+    public ShaderProgram(String vertexSource, String fragmentSource) {
+        int vertexId = compileShader(vertexSource, GL_VERTEX_SHADER);
+        int fragmentId = compileShader(fragmentSource, GL_FRAGMENT_SHADER);
+        programId = glCreateProgram();
+        glAttachShader(programId, vertexId);
+        glAttachShader(programId, fragmentId);
+        glLinkProgram(programId);
+
+        if (glGetProgrami(programId, GL_LINK_STATUS) == GL_FALSE) {
+            throw new IllegalStateException("Shader link failed: " + glGetProgramInfoLog(programId));
+        }
+
+        glDetachShader(programId, vertexId);
+        glDetachShader(programId, fragmentId);
+        glDeleteShader(vertexId);
+        glDeleteShader(fragmentId);
+    }
+
+    public void use() {
+        glUseProgram(programId);
+    }
+
+    public void stop() {
+        glUseProgram(0);
+    }
+
+    public int getId() {
+        return programId;
+    }
+
+    public int getUniformLocation(String name) {
+        return glGetUniformLocation(programId, name);
+    }
+
+    public void setUniformMatrix4(int location, FloatBuffer buffer) {
+        glUniformMatrix4fv(location, false, buffer);
+    }
+
+    public void setUniform1i(int location, int value) {
+        glUniform1i(location, value);
+    }
+
+    public void setUniform3f(int location, float x, float y, float z) {
+        glUniform3f(location, x, y, z);
+    }
+
+    private int compileShader(String source, int type) {
+        int id = glCreateShader(type);
+        glShaderSource(id, source);
+        glCompileShader(id);
+        if (glGetShaderi(id, GL_COMPILE_STATUS) == GL_FALSE) {
+            throw new IllegalStateException("Shader compile failed: " + glGetShaderInfoLog(id));
+        }
+        return id;
+    }
+}
